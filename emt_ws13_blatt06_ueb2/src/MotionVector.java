7c6d47faab7ed9508319c9589bdab9f8cf134560
ATrattnig <trattnig.armin@gmx.at>
2013-12-11 18:01:06 -0800

    ads

--- emt_ws13_blatt06_ueb2/src/MotionVector.java
+++ emt_ws13_blatt06_ueb2/src/MotionVector.java
@@ -34,17 +34,41 @@ public class MotionVector
 	 * @return the motion vector
 	 */
 	private static MotionVector searchMotionVector(int[][] r_frame,	int[][] t_frame, int blocksize, int searchWindow, int row_index, int col_index)
-	{
-		//TODO IMPLEMENT
-		
-		//1. check if the specified macroblock does not exceed the frame border. if so return null.
-		
-		//2. extract the macroblock's data from the t_frame
-				
-		//3. sequential search algorithm on r_frame to determine the motion vector
-			//use MAD function as difference measure.
-				
-		return null;
+	{				
+		double mad = Double.MAX_VALUE;
+		MotionVector result = new MotionVector(0, 0);
+		int p = (searchWindow - 1) / 2;
+
+		// 1. check if the specified macroblock does not exceed the frame
+		// border. if so return null.
+		if (row_index - p <= -p || row_index + p >= r_frame.length)
+			return null;
+
+		if (col_index - p <= -p || col_index + p >= r_frame[0].length)
+			return null;
+
+		// 2. extract the macroblock's data from the t_frame
+		int[][] t_block = new int[blocksize][blocksize];
+		for (int i = 0; i < blocksize; i++)
+			for (int j = 0; j < blocksize; j++) 
+			{
+				t_block[i][j] = t_frame[row_index + i][col_index + j];
+			}
+		// 3. sequential search algorithm on r_frame to determine the motion
+		// vector
+		for (int i = -p; i < p; i++) {
+			for (int j = -p; j < p; j++) {
+				// use MAD function as difference measure.
+				double curr_mad = MAD(t_block, r_frame, i, j, blocksize,
+						row_index, col_index);
+				if (curr_mad < mad) {
+					mad = curr_mad;
+					result.setNewValues(i, j);
+				}
+			}
+		}
+
+		return result;
 	}
 
 	/**
@@ -59,11 +83,23 @@ public class MotionVector
 	 */
 	private static double MAD(int[][] t_block, int[][] r_frame, int i, int j, int blocksize, int row_index, int col_index) 
 	{
-		//TODO IMPLEMENT
 		
 		//calculate the MAD value. if you exceed the r_frame's border return Double.MAX_VALUE;
-		
-		return Double.MAX_VALUE;
+		double sum = 0;
+		for (int k = 0; k < blocksize; k++) 
+		{
+			for (int l = 0; l < blocksize; l++) 
+			{
+				try
+				{
+					sum += Math.abs(t_block[k][l] - r_frame[row_index + k + i][col_index + l + j]);
+				} catch (IndexOutOfBoundsException e) 
+				{
+					return Double.MAX_VALUE;
+				}
+			}
+		}
+		return ((1 / Math.pow(blocksize, 2)) * sum);
 	}
 	
 	/////////////////////////////////////END SECTION FOR STUDENT IMPLEMENTATION\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
